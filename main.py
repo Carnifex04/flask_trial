e3ac6d1a8d45a681b94e5041180e9eae394f5371
hriday <guptahriday4444@gmail.com>
2022-09-28 13:06:02 +0530

    added decorators

--- main.py
+++ main.py
@@ -3,14 +3,38 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(function):
+    def wrapper(*args, **kwargs):
+        return '<b>' + function(*args, **kwargs) + '</b>'
+
+    return wrapper
+
+
+def make_italics(function):
+    def wrapper(*args, **kwargs):
+        return '<i>' + function(*args, **kwargs) + '</i>'
+
+    return wrapper
+
+
+def make_underlined(function):
+    def wrapper(*args, **kwargs):
+        return '<u>' + function(*args, **kwargs) + '</u>'
+
+    return wrapper
+
+
 @app.route('/')
 def hello_world():
     return "My Name is Hriday Gupta!"
 
 
-@app.route('/username/<name>')
-def greet(name):
-    return f"Hi {name}!"
+@app.route('/username/<name>/<surname>')
+@make_italics
+@make_bold
+@make_underlined
+def greet(name, surname):
+    return f"Hi {name} {surname}!"
 
 
 if __name__ == '__main__':
